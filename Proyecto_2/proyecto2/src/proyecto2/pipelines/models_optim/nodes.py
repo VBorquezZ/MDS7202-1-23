252e8dc25fc83fb296c33de8e9eb7f7b0c61ec9d
Vicente1983B <borquezvicente1983@gmail.com>
2023-07-23 04:11:40 -0400

    Model Optimization Pipeline

--- Proyecto_2/proyecto2/src/proyecto2/pipelines/models_optim/nodes.py
+++ Proyecto_2/proyecto2/src/proyecto2/pipelines/models_optim/nodes.py
@@ -2,3 +2,99 @@
 This is a boilerplate pipeline 'models_optim'
 generated using Kedro 0.18.11
 """
+import logging
+
+import numpy as np
+import pandas as pd
+
+from sklearn.pipeline import Pipeline
+
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+
+from sklearn.metrics import make_scorer, recall_score
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import classification_report
+
+log = logging.getLogger(__name__)
+
+def optim_best_models(ColumnTransformer, X_train, y_train, X_val, y_val):
+
+    XGB_Opt_pipe = Pipeline([
+        ("preprocesamiento", ColumnTransformer),
+        ("XGB_OPT", XGBClassifier())
+    ], memory = ".")
+
+    p_grid_XGB = [
+        {
+            "XGB_OPT__n_estimators": [100,
+                                       150,
+                                       200],
+            "XGB_OPT__booster" : ["gbtree", 
+                                  "gblinear", 
+                                  "dart"],
+        }
+    ]
+    
+             
+    LGBM_Opt_pipe = Pipeline([
+        ("preprocesamiento", ColumnTransformer),
+        ("LGBM_OPT", LGBMClassifier())
+    ], memory = ".")
+
+    p_grid_LGBM = [
+        {
+            "LGBM_OPT__boosting_type": ["dart", 
+                                         "gbdt", 
+                                         #"rf"
+                                        ],
+            "LGBM_OPT__num_leaves": [31,
+                                     40,
+                                     50,
+                                     60,
+                                     70
+                                    ],
+            "LGBM_OPT__n_estimators": [100,
+                                       150,
+                                       ],
+                                           
+        }
+    ]
+    
+    scorer = make_scorer(recall_score, pos_label=1)
+
+    # Crear el objeto GridSearchCV con la métrica recall de la clase '1'
+    gsXGB = GridSearchCV(XGB_Opt_pipe, 
+                        p_grid_XGB, 
+                        scoring=scorer, 
+                        n_jobs=-1)
+    
+    gsLGBM = GridSearchCV(LGBM_Opt_pipe, 
+                          p_grid_LGBM,
+                          scoring=scorer, 
+                          n_jobs=-1)
+
+    # Ajustar el GridSearchCV a tus datos de entrenamiento
+    gsXGB = gsXGB.fit(X_train, y_train.values.ravel())
+    gsLGBM = gsLGBM.fit(X_train, y_train.values.ravel())
+
+    log.info("gsDT best score: " + str(gsXGB.best_score_))
+    log.info("gsDT best params: " + str(gsXGB.best_params_))
+    log.info("gsLGBM best score: " + str(gsLGBM.best_score_))
+    log.info("gsLGBM best params: " + str(gsLGBM.best_params_))
+
+    y_predxgb = gsXGB.predict(X_val)
+    y_predlgbm = gsLGBM.predict(X_val)
+    reportXGB = classification_report(y_val, y_predxgb, output_dict=True)
+    reportLGBM = classification_report(y_val, y_predlgbm, output_dict=True)
+    reportXGB = pd.DataFrame(reportXGB)
+    reportLGBM = pd.DataFrame(reportLGBM)
+    return gsXGB, gsLGBM, reportXGB, reportLGBM
+
+def evaluate_pipe(features, labels, pipeline):
+    y_pred = pipeline.predict(features)
+    report = pd.DataFrame(classification_report(labels, 
+                                                y_pred, 
+                                                output_dict=True))
+    return report
--- Proyecto_2/proyecto2/src/proyecto2/pipelines/models_optim/pipeline.py
+++ Proyecto_2/proyecto2/src/proyecto2/pipelines/models_optim/pipeline.py
@@ -4,7 +4,39 @@ generated using Kedro 0.18.11
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-
+from .nodes import (
+    optim_best_models,
+    evaluate_pipe
+    
+)
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return Pipeline(
+        [
+            node(
+                func=optim_best_models,
+                inputs=["Col_Transformer",
+                        "X_train",
+                        "y_train",
+                        "X_valid",
+                        "y_valid"],
+                outputs=["XGB_Opt",
+                         "LGBM_Opt",
+                         "reportXGB", 
+                         "reportLGBM"],
+                name="optim_models",
+            ), 
+
+            node(
+                func=evaluate_pipe,
+                inputs=["X_test",
+                        "y_test",
+                        "XGB_Opt"],
+                outputs="XGB_report_Test",
+                name="eval_best_model",
+            ), 
+
+
+            
+        ]
+    )
